4c2d52eea491391c91626590bc399b8252cded49
hendraaagil <hendraaagil@gmail.com>
2021-01-04 20:53:56 +0700

    Tipe Data Array

---
+++ build/array.js
@@ -0,0 +1,11 @@
+"use strict";
+// Array
+var array = [1, 2, 3];
+array = [4, 5, 6, 7, 8];
+var array2;
+array2 = ['Nama', 'Saya', 'Hendra'];
+var array3;
+array3 = ['Agil', 'Umur:', 17, 'Menikah?', false];
+console.log(array.join(', '));
+console.log(array2.join(' '));
+console.log(array3.join(' '));
---
+++ src/array.ts
@@ -0,0 +1,13 @@
+// Array
+let array: number[] = [1, 2, 3];
+array = [4, 5, 6, 7, 8];
+
+let array2: string[];
+array2 = ['Nama', 'Saya', 'Hendra'];
+
+let array3: any[];
+array3 = ['Agil', 'Umur:', 17, 'Menikah?', false];
+
+console.log(array.join(', '));
+console.log(array2.join(' '));
+console.log(array3.join(' '));
