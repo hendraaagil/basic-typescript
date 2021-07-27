9cb59d4efdd5df5c8e86c83fc17972c997b46587
hendraaagil <hendraaagil@gmail.com>
2021-07-27 21:56:31 +0700

    Tipe Data Object

---
+++ build/object.js
@@ -0,0 +1,12 @@
+"use strict";
+// Object
+var user = {
+    name: 'Hendra Agil',
+    age: 17,
+    address: 'Karanganyar'
+};
+user = {
+    name: 'Syaputra',
+    age: 18,
+    address: 'Surakarta'
+};
---
+++ src/object.ts
@@ -0,0 +1,19 @@
+// Object
+
+type User = {
+  name: string,
+  age: number,
+  address: string
+}
+
+let user: User = {
+  name: 'Hendra Agil',
+  age: 17,
+  address: 'Karanganyar'
+}
+
+user = {
+  name: 'Syaputra',
+  age: 18,
+  address: 'Surakarta'
+}
